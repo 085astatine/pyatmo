a6106189ba90c87fba475e47aae1838872476c79
Astatine <astatine085@gmail.com>
2018-09-26 23:01:43 +0900

    get tokens using username & password

---
+++ pyatmo/_oauth.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+import logging
+from typing import List, Optional
+import requests
+
+
+_token_url = 'https://api.netatmo.com/oauth2/token'
+
+
+class OAuth:
+    def __init__(
+            self,
+            client_id: str,
+            client_secret: str,
+            logger: Optional[logging.Logger] = None) -> None:
+        # logger
+        self._logger = logger or logging.getLogger(__name__)
+        # client
+        self._client_id = client_id
+        self._client_secret = client_secret
+        # token
+        self._access_token: Optional[str] = None
+        self._refresh_token: Optional[str] = None
+
+    def get_access_token(
+            self,
+            username: str,
+            password: str,
+            scope_list: List[str]) -> None:
+        self._logger.info('get access token')
+        data = {'grant_type': 'password',
+                'client_id': self._client_id,
+                'client_secret': self._client_secret,
+                'username': username,
+                'password': password,
+                'scope': ' '.join(scope_list)}
+        response = requests.post(_token_url, data=data)
+        if response.ok:
+            self._logger.info('get access token: success')
+            result = response.json()
+            self._access_token = result['access_token']
+            self._refresh_token = result['refresh_token']
+        else:
+            self._logger.error('get access token: failed')
