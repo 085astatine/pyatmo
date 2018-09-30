337e634ed9e46455b8b01d7b1a77034dc975871c
Astatine <astatine085@gmail.com>
2018-09-30 23:16:56 +0900

    modify Scope
    - def __str__()
    - def default()
    - set value to enum.auto()

--- pyatmo/_scope.py
+++ pyatmo/_scope.py
@@ -4,12 +4,19 @@ import enum
 
 
 class Scope(enum.Enum):
-    READ_STATION = 'read_station'
-    READ_THERMOSTAT = 'read_thermostat'
-    WRITE_THERMOSTAT = 'write_thermostat'
-    READ_CAMERA = 'read_camera'
-    WRITE_CAMERA = 'write_camera'
-    ACCESS_CAMERA = 'accessss_camera'
-    READ_PRESENCE = 'read_presence'
-    ACCESS_PRESENCE = 'access_presence'
-    READ_HOMECOACH = 'read_homecoach'
+    READ_STATION = enum.auto()
+    READ_THERMOSTAT = enum.auto()
+    WRITE_THERMOSTAT = enum.auto()
+    READ_CAMERA = enum.auto()
+    WRITE_CAMERA = enum.auto()
+    ACCESS_CAMERA = enum.auto()
+    READ_PRESENCE = enum.auto()
+    ACCESS_PRESENCE = enum.auto()
+    READ_HOMECOACH = enum.auto()
+
+    def __str__(self) -> str:
+        return self.name.lower()
+
+    @classmethod
+    def default(cls) -> 'Scope':
+        return cls.READ_STATION
