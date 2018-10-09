8b931bada27908189f755a85baaf96d49439d0a3
Astatine <astatine085@gmail.com>
2018-10-09 23:36:12 +0900

    use sqlalchemy

--- pyatmo/weather/_device.py
+++ pyatmo/weather/_device.py
@@ -1,45 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import sqlalchemy
+from ._sqlalchemy import _DeclarativeBase
 
-class Device:
-    def __init__(
-            self,
-            id: str,
-            name: str,
-            latitude: float,
-            longitude: float,
-            altitude: float) -> None:
-        self._id = id
-        self._name = name
-        self._latitude = latitude
-        self._longitude = longitude
-        self._altitude = altitude
 
-    @property
-    def id(self) -> str:
-        return self._id
-
-    @property
-    def name(self) -> str:
-        return self._name
-
-    @property
-    def latitude(self) -> float:
-        return self._latitude
-
-    @property
-    def longitude(self) -> float:
-        return self._longitude
-
-    @property
-    def altitude(self) -> float:
-        return self._altitude
-
-    @classmethod
-    def create_table_command(cls):
-        return ('create table devices'
-                '(id text primary key,'
-                ' name text,'
-                ' latitude real,'
-                ' longitude real,'
-                ' altitude real)')
+class Device(_DeclarativeBase):
+    # table naame
+    __tablename__ = 'devices'
+    # column
+    id = sqlalchemy.Column(sqlalchemy.String, primary_key=True)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    latitude = sqlalchemy.Column(sqlalchemy.Float)
+    longitude = sqlalchemy.Column(sqlalchemy.Float)
+    altitude = sqlalchemy.Column(sqlalchemy.Float)
---
+++ pyatmo/weather/_sqlalchemy.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+import sqlalchemy.ext.declarative
+
+
+_DeclarativeBase = sqlalchemy.ext.declarative.declarative_base()
