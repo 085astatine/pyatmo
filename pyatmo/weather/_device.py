b84bdd1656d1fcef204f15fb2977c15234bc3fb6
Astatine <astatine085@gmail.com>
2018-10-10 22:10:38 +0900

    add Module class

--- pyatmo/weather/_device.py
+++ pyatmo/weather/_device.py
@@ -13,3 +13,24 @@ class Device(_DeclarativeBase):
     latitude = sqlalchemy.Column(sqlalchemy.Float)
     longitude = sqlalchemy.Column(sqlalchemy.Float)
     altitude = sqlalchemy.Column(sqlalchemy.Float)
+
+
+class Module(_DeclarativeBase):
+    # table name
+    __tablename__ = 'modules'
+    # column
+    id = sqlalchemy.Column(sqlalchemy.String, primary_key=True)
+    device_id = sqlalchemy.Column(
+            sqlalchemy.String,
+            sqlalchemy.ForeignKey('devices.id'))
+    name = sqlalchemy.Column(sqlalchemy.String)
+    module_type = sqlalchemy.Column(sqlalchemy.String)
+    data_type = sqlalchemy.Column(sqlalchemy.String)
+    # relationship
+    device = sqlalchemy.orm.relationship('Device', back_populates='modules')
+
+
+Device.modules = sqlalchemy.orm.relationship(
+        'Module',
+        order_by=Module.id,
+        back_populates='device')
