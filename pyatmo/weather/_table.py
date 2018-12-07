ed17fc2276be0a5407747252e6cd1a7d6766c84b
Astatine <astatine085@gmail.com>
2018-12-07 17:31:32 +0900

    add measurements table

--- pyatmo/weather/_table.py
+++ pyatmo/weather/_table.py
@@ -58,3 +58,38 @@ class Module(_DeclarativeBase):
                         '{0}={1}'.format(column.key,
                                          repr(getattr(self, column.key)))
                         for column in mapper.column_attrs))
+
+
+class Measurements(_DeclarativeBase):
+    # table name
+    __tablename__ = 'measurements'
+    # column
+    timestamp = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    module_id = sqlalchemy.Column(
+            sqlalchemy.String,
+            sqlalchemy.ForeignKey('modules.id'),
+            primary_key=True)
+    temperature = sqlalchemy.Column(sqlalchemy.Float)
+    humidity = sqlalchemy.Column(sqlalchemy.Float)
+    pressure = sqlalchemy.Column(sqlalchemy.Float)
+    co2 = sqlalchemy.Column(sqlalchemy.Float)
+    noise = sqlalchemy.Column(sqlalchemy.Float)
+    rain = sqlalchemy.Column(sqlalchemy.Float)
+    wind_strength = sqlalchemy.Column(sqlalchemy.Float)
+    wind_angle = sqlalchemy.Column(sqlalchemy.Float)
+    gust_strength = sqlalchemy.Column(sqlalchemy.Float)
+    gust_angle = sqlalchemy.Column(sqlalchemy.Float)
+    # replationship
+    module = sqlalchemy.orm.relationship(
+            'Module',
+            lazy='immediate')
+
+    def __repr__(self) -> str:
+        mapper = sqlalchemy.inspect(self.__class__)
+        return '{0}.{1}({2})'.format(
+                self.__class__.__module__,
+                self.__class__.__name__,
+                ', '.join(
+                        '{0}={1}'.format(column.key,
+                                         repr(getattr(self, column.key)))
+                        for column in mapper.column_attrs))
