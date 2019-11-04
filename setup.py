bff93813bd08296d272ec5800f69feca8959a1dd
Astatine <astatine085@gmail.com>
2019-11-04 22:50:39 +0900

    Use setup.cfg

--- setup.py
+++ setup.py
@@ -2,17 +2,5 @@
 
 from setuptools import setup
 
-setup(
-    name='pyatmo',
-    version='0.0.0',
-    author='Astatine',
-    author_email='astatine085@gmail.com',
-    url='https://github.com/085astatine/pyatmo',
-    packages=[
-        'pyatmo',
-        'pyatmo.weather'],
-    install_requires=[
-        'pytz',
-        'pyyaml>=4.2b1',
-        'requests>=2.20',
-        'sqlalchemy'])
+
+setup()
